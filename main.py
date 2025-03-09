a05fa63e5ca1bf2038929632920ebfdf62c3ad27
sandeepbandi924 <bandisandeep1374@gmail.com>
2025-03-09 17:10:10 +0530

    model trainer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ChestCancerClassifier import logger
 from ChestCancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from ChestCancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from ChestCancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from ChestCancerClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -49,14 +49,14 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Evaluation stage"
-try:
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   model_evalution = EvaluationPipeline()
-   model_evalution.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# STAGE_NAME = "Evaluation stage"
+# try:
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_evalution = EvaluationPipeline()
+#    model_evalution.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-except Exception as e:
-        logger.exception(e)
-        raise e
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
